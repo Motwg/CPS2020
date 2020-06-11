559d7ad2e3baf4fe3bb2eb78a2c3b6963a6c3837
Motwg <38580353+Motwg@users.noreply.github.com>
2020-06-11 13:44:02 +0200

    Added complex numbers in plot

--- core/utils.py
+++ core/utils.py
@@ -1,3 +1,6 @@
+import cmath
+
+
 def multiply_vectors(v1, v2):
     if len(v2) > len(v1):
         v1, v2 = v2, v1
@@ -14,3 +17,25 @@ def extend(v, length):
     while len(v) < length:
         v.append(v[-1] + v[1] - v[0])
     return v
+
+
+def split_complex(vector, w):
+    if isinstance(vector, list) and len(vector) > 0:
+        if isinstance(vector[0], complex):
+            if w == 'W1':
+                real, imag = [], []
+                for v in vector:
+                    real.append(v.real)
+                    imag.append(v.imag)
+                return real, imag
+            else:
+                mod, arg = [], []
+                for v in vector:
+                    pol = cmath.polar(v)
+                    mod.append(pol[0])
+                    arg.append(pol[1])
+                return mod, arg
+        else:
+            return vector, [0] * len(vector)
+    else:
+        raise Exception('Argument should be not empty list')
--- gui/plot.py
+++ gui/plot.py
@@ -1,15 +1,18 @@
 import matplotlib.pyplot as plt
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QDialog, QPushButton, QVBoxLayout, QSlider, QHBoxLayout, QLabel
+from PyQt5.QtWidgets import QDialog, QPushButton, QVBoxLayout, QSlider, QHBoxLayout, QLabel, QComboBox
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 
 from core import statistics as st
+from core.merging import perform_merge
+from core.utils import split_complex
 
 
 class Plot(QDialog):
     def __init__(self, parent=None):
         super(Plot, self).__init__(parent)
+        self.resize(1200, 600)
 
         self.figure = plt.figure(figsize=(10, 4))
         # this is the Canvas Widget that displays the `figure`
@@ -32,43 +35,65 @@ class Plot(QDialog):
         self.sl_bins.setTickInterval(0)
         self.sl_bins.valueChanged.connect(self.update)
 
-        # set the layout
-        self.upper_layout = QVBoxLayout()
-        self.create_labels()
+        self.cb_plot = QComboBox(self)
+        self.cb_plot.addItems(['W1', 'W2'])
+        self.cb_plot.currentIndexChanged.connect(self.update)
+
+        self.cb_hist = QComboBox(self)
+        self.cb_hist.addItems(['Img plot', 'Histogram'])
+        self.cb_hist.currentIndexChanged.connect(self.update)
+
+        # set the upper layout
+        upper_layout = QHBoxLayout()
+        upper_layout.addLayout(self.create_labels())
+        upper_right_layout = QVBoxLayout()
+        upper_right_layout.addWidget(self.cb_plot)
+        upper_right_layout.addWidget(self.cb_hist)
+        upper_layout.addLayout(upper_right_layout)
+
+        # set overall layout
         layout = QVBoxLayout()
         layout.addWidget(self.toolbar)
-        layout.addLayout(self.upper_layout)
+        layout.addLayout(upper_layout)
         layout.addWidget(self.canvas)
-
         layout.addWidget(self.sl_bins)
         layout.addWidget(self.button)
         self.setLayout(layout)
+
         self.show_main = True
-        self.vector_x, self.vector_y, self.alg1, self.alg2, self.merge_method = None, None, None, None, None
+        self.vector_x, self.vector_y, self.vector_i = None, None, None
+        self.alg1, self.alg2, self.merge_method = None, None, None
         self.extras = [None, None, None]
 
     def update(self):
-        if self.alg2 is None:
-            self.vector_x, self.vector_y = self.alg1.perform_algorithm()
-        else:
-            self.vector_x, self.vector_y = self.merge_method(self.alg1, self.alg2)
         self.figure.clear()
+        self.vector_x, self.vector_y = perform_merge(self.alg1, self.alg2, self.merge_method)
+        self.vector_y, self.vector_i = split_complex(self.vector_y, self.cb_plot.currentText())
 
         # plot
-        ax = self.figure.add_subplot(121)
+        ax = self.figure.add_subplot(211)
+        ax_i = self.figure.add_subplot(212)
         if self.show_main:
             ax.plot(self.vector_x, self.vector_y, linestyle='-', marker='o', color='red', markersize=1.4)
+            ax.grid(True)
+            ax.margins(0.01, 0.05)
+            ax.set_xlabel('t[s]')
+
+            if self.cb_hist.currentText() == 'Img plot':
+                # imaginary plot
+                ax_i.plot(self.vector_x, self.vector_i, linestyle='-', marker='o', color='red', markersize=1.4)
+                ax_i.grid(True)
+                ax_i.margins(0.01, 0.05)
+                ax_i.set_xlabel('t[s]')
+            else:
+                # histogram
+                ax_i = self.figure.add_subplot(313)
+                ax_i.hist(self.vector_y, bins=self.sl_bins.value())
+                ax_i.grid(True)
+
         for extra in self.extras:
             if extra is not None:
                 ax.plot(extra[0], extra[1], **extra[2])
-        ax.grid(True)
-        ax.margins(0.01, 0.05)
-        ax.set_xlabel('t[s]')
-
-        # histogram
-        ax = self.figure.add_subplot(122)
-        ax.hist(self.vector_y, bins=self.sl_bins.value())
-        ax.grid(True)
 
         self.canvas.draw()
         self.update_labels()
@@ -84,6 +109,7 @@ class Plot(QDialog):
 
     def create_labels(self):
         labs = []
+        upper_left_layout = QVBoxLayout()
         for lab in ['Średnia: ', 'Bezwględna średnia: ', 'Wartość skuteczna: ', 'Wariancja: ', 'Moc średnia: ']:
             l_lab = QLabel()
             l_lab.setAlignment(Qt.AlignLeft)
@@ -95,40 +121,42 @@ class Plot(QDialog):
         mean_layout = QHBoxLayout()
         mean_layout.addWidget(labs[0])
         mean_layout.addWidget(self.l_mean)
-        mean_layout.addSpacing(600)
-        self.upper_layout.addLayout(mean_layout)
+        mean_layout.addSpacing(900)
+        upper_left_layout.addLayout(mean_layout)
 
         self.l_absmean = QLabel()
         self.l_absmean.setAlignment(Qt.AlignLeft)
         abs_layout = QHBoxLayout()
         abs_layout.addWidget(labs[1])
         abs_layout.addWidget(self.l_absmean)
-        abs_layout.addSpacing(600)
-        self.upper_layout.addLayout(abs_layout)
+        abs_layout.addSpacing(900)
+        upper_left_layout.addLayout(abs_layout)
 
         self.l_ws = QLabel()
         self.l_ws.setAlignment(Qt.AlignLeft)
         ws_layout = QHBoxLayout()
         ws_layout.addWidget(labs[2])
         ws_layout.addWidget(self.l_ws)
-        ws_layout.addSpacing(600)
-        self.upper_layout.addLayout(ws_layout)
+        ws_layout.addSpacing(900)
+        upper_left_layout.addLayout(ws_layout)
 
         self.l_war = QLabel()
         self.l_war.setAlignment(Qt.AlignLeft)
         war_layout = QHBoxLayout()
         war_layout.addWidget(labs[3])
         war_layout.addWidget(self.l_war)
-        war_layout.addSpacing(600)
-        self.upper_layout.addLayout(war_layout)
+        war_layout.addSpacing(900)
+        upper_left_layout.addLayout(war_layout)
 
         self.l_ms = QLabel()
         self.l_ms.setAlignment(Qt.AlignLeft)
         ms_layout = QHBoxLayout()
         ms_layout.addWidget(labs[4])
         ms_layout.addWidget(self.l_ms)
-        ms_layout.addSpacing(600)
-        self.upper_layout.addLayout(ms_layout)
+        ms_layout.addSpacing(900)
+        upper_left_layout.addLayout(ms_layout)
+
+        return upper_left_layout
 
     def update_labels(self):
         self.l_mean.setText(str(round(st.mean(self.vector_y), 2)))
